eb6bb37a5fb4a2d62f32ec7dcedc4d3ac8580cb8
Khelden <cottertr1@gmail.com>
2023-05-24 18:51:38 +0930

    removed-testing

--- pages/404.js
+++ pages/404.js
@@ -15,7 +15,7 @@ export default function NotFound() {
     return (
         <section className="showcase" id="home">  
         <div className="showcase_container container">
-          <h1 className="showcase_title title title-main">404 Error TESTING</h1>
+          <h1 className="showcase_title title title-main">404 Error</h1>
           <h1 className="showcase_title title title-main">Oooops!</h1>
           <h1 className="showcase_title title title-main">That page cannot be found :( </h1>
           <p>Redirecting to the <Link href="/"><a className="homepage-404-style">Homepage</a></Link></p>
